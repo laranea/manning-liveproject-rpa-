cf59c2237e72a0e7f87bf5ae2283958e06c4006a
laranea <kristof.leroux@gmail.com>
2020-06-04 16:32:29 +0200

    getfiles

--- getpdfs.py
+++ getpdfs.py
@@ -6,7 +6,6 @@ def getfiles():
     # Instruction(s) to get the list of PDF files goes here (and/or below)...
     pdf_files = []
     for root, dirs, files in os.walk(os.getcwd()):
-        pdf_files.extend([])
         for fname in files:
             if fname.lower().endswith('.pdf'):
                 pdf_files.append(os.path.join(root, fname))
